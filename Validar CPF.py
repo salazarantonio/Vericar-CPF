75d3ab0c4b2f64ee7fda1b85d3101c5524a836e5
Salazar <antoniojbsalazar@gmai.com>
2026-03-11 23:58:13 -0300

    publicacao = "Olá, pessoal! Hoje quero compartilhar com vocês um código em Python que desenvolvi para validar CPF (Cadastro de Pessoas Físicas) no Brasil. O CPF é um número de identificação fiscal utilizado para pessoas físicas, e é importante garantir que ele seja válido antes de utilizá-lo em qualquer sistema ou aplicação.

---
+++ Validar CPF.py
@@ -0,0 +1,54 @@
+def validar(cpf):
+    if len(cpf) != 14:
+        print("O formato do CPF é inválido, siga o formato 'xxx.xxx.xxx-xx'.")
+        return False
+    elif cpf[3] != '.' or cpf[7] != '.' or cpf[11] != '-':
+        print("O formato do CPF é inválido, siga o formato 'xxx.xxx.xxx-xx'.")
+        return False
+    
+    rpl = cpf.replace(".", "").replace("-", "")
+    if not rpl.isdigit() or len(rpl) != 11:
+        print("O CPF deve conter apenas números, traço e número, seguindo o formato 'xxx.xxx.xxx-xx'.")
+        return False
+    
+    numeros = []
+    for i in rpl:
+        numeros.append(int(i))
+
+# Primeiro dígito verificador
+    soma = 0
+    for i in range(9):
+        soma += numeros[i] * (10 - i)
+    resto = soma % 11
+    if resto < 2:
+        d1 = 0
+    else:
+        d1 = 11 - resto
+
+ # Verificação 1
+    if d1 != numeros[9]:
+        print("CPF inválido")
+        return False
+
+# Segundo dígito verificador
+    soma = 0
+    for i in range(10):
+        soma += numeros[i] * (11 - i)
+    resto = soma % 11
+    if resto < 2:
+        d2 = 0
+    else:
+        d2 = 11 - resto
+
+# Verificação 2
+    if d2 != numeros[10]:
+        print("CPF inválido")
+        return False
+    
+    print("CPF válido")
+    return True
+
+# void main() {
+cpf_input = input("Digite um CPF no formato xxx.xxx.xxx-xx: ")
+validar(cpf_input)
+# }
